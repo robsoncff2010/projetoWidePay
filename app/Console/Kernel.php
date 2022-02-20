c3f942bd6fd7059bfa898f3ed1bf1aa817d620ca
Robson Camilo <rcamilo@kryptontech.com.br>
2022-02-20 20:28:26 -0300

    teste cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('url:cron')->everyMinute();
+        // $schedule->command('url:cron')->everyMinute();
     }
 
     /**
